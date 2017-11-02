fa44bb30067ec261442440911d8bb677f2f8a876
Ria Nicole Carmin <akimalunar@gmail.com>
2017-11-01 21:12:52 -0700

    Added an example test

--- src/routes/home/Home.test.js
+++ src/routes/home/Home.test.js
@@ -3,9 +3,7 @@ import Home from './index';
 import { shallow } from 'preact-render-spy'; // See: https://github.com/mzgoddard/preact-render-spy
 // import { shallow, deep } from 'preact-render-spy'; // See: https://github.com/mzgoddard/preact-render-spy
 
-test('check if Home is rendering', () => {
-  // Use shallow (rendering) if the level of rendering is just 1, e.g. <Headline> is returning already HTML and not another VDOM element
-  // which itself calls a render method (this would be level 2)
+test('Home is rendering', () => {
   const actual = shallow(<Home />);
   expect(actual.find('div').length).toBe(1);
 });
